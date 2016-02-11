1aa533337ebf77c8b7f0aa217cc1469b84cbe55f
IT. Student <it.student@ITSB.LOCAL>
2016-02-11 11:48:36 -0800

    first test commit

---
+++ src/com/example/hwapp/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.hwapp;
+
+/**
+ * Created by it.student on 2/11/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
