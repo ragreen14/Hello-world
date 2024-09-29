2634cfed9a0b283105c782ae5ea5d27ff3e6966f
Rob Green <ragreen@dmacc.edu>
2024-09-29 17:59:40 -0500

    Fork assignment commit

---
+++ clonepj/src/main/java/com/mycompany/clonepj/Clonepj.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clonepj;
+
+/**
+ *
+ * @author Rober
+ */
+public class Clonepj {
+/*Addition by Rob Green*/
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
